f6687f63f33831b827c4ad4eb043532a01fd664d
mani <emanimanikanta3@gmail.com>
2025-12-19 00:41:33 +0530

    JSC-1: Initial Spring Boot project setup

---
+++ src/main/java/com/jaguar/servicecenter/JaguarServiceCenterApiApplication.java
@@ -0,0 +1,13 @@
+package com.jaguar.servicecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaguarServiceCenterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaguarServiceCenterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaguar/servicecenter/JaguarServiceCenterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaguar.servicecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaguarServiceCenterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
